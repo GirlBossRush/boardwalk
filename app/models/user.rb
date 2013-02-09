a2bb8ade9db2151d8147d6647ac84eaadb29e367
Eric Wright <dev@ericwright.me>
2013-02-09 14:31:00 -0500

    Add validations for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,22 @@
 class User
   include Mongoid::Document
+  include ActiveModel::SecurePassword
   has_secure_password
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation
 
+  field :username
   field :email, type: String
   field :password_digest, type: String
 
-  validates_uniqueness_of :email
+  validates(:username,
+            presence: true,
+            format: /^[A-Za-z\-\d_]+$/, # Alphanumeric, underscores and dashes
+            uniqueness: { case_sensitive: false })
+
+  validates(:password,
+            presence: { on: :create })
+
+  validates(:email,
+            uniqueness: { case_sensitive: false },
+            format: /.+@.+\..+/i) # Just an '@' and a '.'
 end
