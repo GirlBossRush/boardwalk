d681d7758d40c34c4b28694bb81cb861d5d9f75d
Eric Wright <dev@ericwright.me>
2013-02-09 16:37:23 -0500

    Add user form to front page. Fix various styling issues.
    * Add form styling.
    * Add link styling.
    * Add repository link.
    * Add additional header styles.
    * Clean up session methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   private
     def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      if session[:user_id]
+        @current_user ||= User.find_by_id(session[:user_id])
+      end
     end
-    helper_method :current_user
 
-    def authorize
-      redirect_to login_url, alert: "Not authorized" if current_user.nil?
-    end
+    helper_method :current_user
   #end_private
 end
