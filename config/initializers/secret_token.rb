4ade57faa3d8a5a3ed37b708bf03b025cd373101
Eric Wright <dev@ericwright.me>
2013-02-19 16:55:50 -0500

    Check for secret cookie token in production.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,18 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Boardwalk::Application.config.secret_token = 'e2cfeddf2bae1b9c0c813a9556a0f0cb1b5a3c808ff8dd99f1d5ca1273d93190f53bd0daadae5e3182e0e60c3dd3727434a3a82a7d8bcbfe74014ef280373027'
+
+
+SECRET_COOKIE_TOKEN = if Rails.env.production?
+                        ENV['SECRET_COOKIE_TOKEN']
+                      else
+                        "f23897532768a54b3e0049b49feb2718a28c76d22ffcdc05440b8b6c8f25b9d0eb3288ae7c4e04b175b49ed359b6bdfbfe7899606ce7f01d641fa00fb9383114"
+                      end
+
+if SECRET_COOKIE_TOKEN.nil? || SECRET_COOKIE_TOKEN.blank?
+  puts "Secret cookie token not set!"
+  puts "Run heroku config:add SECRET_COOKIE_TOKEN=YOUR_SECRET_KEY"
+  exit
+else
+  Boardwalk::Application.config.secret_token = SECRET_COOKIE_TOKEN
+end
