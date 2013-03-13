6b50289bd83494361a30f7594cddd58624513080
Eric Wright <dev@ericwright.me>
2013-03-12 20:29:23 -0400

    Fix issue where bad sessions logins would not return correct errors.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   respond_to :json
 
   def create
-    user = User.find_by(email:params[:user][:email])
+    user = User.where(email: params[:user][:email]).first
 
     if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
