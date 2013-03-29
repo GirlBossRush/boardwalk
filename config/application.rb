bbe679e8e0eaa3851bf8120c03137d0947d9612c
Eric Wright <dev@ericwright.me>
2013-03-29 18:53:01 -0400

    Remove unnecessary Mongoid Railtie.

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
 # require "active_record/railtie"
-require "mongoid/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
